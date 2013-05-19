c916c36dba090f42ed711c0010fce1cbe8dd27dc
Lyn Headley <laheadle@gmail.com>
2013-05-19 01:01:33 +0100

    add keybindings for difficulty level switch

--- www/js/difficultyView.js
+++ www/js/difficultyView.js
@@ -4,26 +4,44 @@ define(['underscore', 'backbone', 'jquery', 'message', 'text!tmpl/difficulty.ejs
     return Backbone.View.extend({
         template: _.template(template),
         id: "difficulty",
+        tagName: "span",
         events: {
-            "click .increment" : function() {
-                State.set('difficulty', State.get('difficulty') + 1)
-                return false
-            },
-
-            "click .decrement" : function() {
-                State.set('difficulty', State.get('difficulty') - 1)
-                return false
-            }
+            "click .increment" : "increment",
+            "click .decrement" : "decrement",
+            'keyup': 'doKey',
         },
 
         initialize: function() {
             this.listenTo(State, 'change', this.render)
+            var that = this;
+            $('body').keyup(function(e){ that.doKey(e) });
+        },
+
+        increment: function() {
+            State.set('difficulty', State.get('difficulty') + 1)
+            return false
+        },
+
+        decrement: function() {
+            State.set('difficulty', State.get('difficulty') - 1)
+            return false
         },
 
         render: function() {
             this.$el.html(this.template(State.toJSON()));
             return this
         },
+
+        doKey: function(e) {
+            switch (e.keyCode) {
+            case 37://left
+                this.decrement()
+                break;
+            case 39://right
+                this.increment()
+                break;
+            }
+        }
 
     }) 
 })
