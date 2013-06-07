373d555ebbd560a4554c73063458ec717a44241e
Lyn Headley <laheadle@gmail.com>
2013-06-07 04:05:52 +0100

    remove old scripts

--- gen.sh
+++
@@ -1,9 +0,0 @@
-cd sheets
-
-python ../sm-json.py reformation
-python ../sm-json.py ap-ap
-python ../sm-json.py ap-businessweek
-python ../sm-json.py history
-python ../sm-json.py pentagon
-python ../sm-json.py propublica-nowarrant
-
